7e68b5d66ae18d87a66add5bb3f7494bcc1b2bfe
ejonghwan <sun87-1@daum.net>
2024-06-10 19:25:55 +0900

    넥스트 테일윈드 연습

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+// rafce
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py12 text-white bg-pink-50/20">
+            
+            {/* logo */}
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Jong<span className="text-accent">.</span>
+                    </h1>
+                </Link>
+
+                 {/* desktop Nav */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href={'/content'}>
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* mobile Nav */}
+                <div className="xl:hidden ">
+                    <Nav />
+                    <Link href={'/content'}>
+                        mo
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+            </div>
+
+
+           
+        </header>
+    )
+}
+
+export default Header;
