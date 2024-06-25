b932fc28fda0109c21f92dfa1df86b57752c2b82
ejonghwan <sun87-1@daum.net>
2024-06-25 17:00:32 +0900

    a

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,51 @@
 import React from 'react'
 
 const ContactPage = () => {
+
+
+  interface Props {
+    a: string,
+    b: number
+  }
+  const abc = [] as Props[]
+
+
+  // function aa(a, b) {
+  //   return a + b
+  // }
+
+  // aa(3, 5) === 8
+
+  [{
+    data: [{name: 'jonghwan', age: 3}],
+    status: 200,
+    message: '성공'
+  }]
+
+
+  interface P1<T> {
+    p1: T
+  }
+
+  interface P2<T> {
+    p2: T
+  }
+
+  const obj: P1<P2<string>> = {
+    p1: {
+      p2: 'asdasd'
+    }
+  }
+
+
+  function ge<T>(arr: T): T {
+    return arr
+  }
+
+  ge<number[]>([1,2,'b'])
+
+
+
   return (
     <div>ContactPage</div>
   )
