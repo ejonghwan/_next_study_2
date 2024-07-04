383c50a442c1fdc1d3bd7b16a4666d18e64faa82
ejonghwan <sun87-1@daum.net>
2024-07-04 11:15:09 +0900

    a

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import RefParent from '@/components/RefParent'
 import AsyncCompo from '@/components/AsyncCompo'
 import ContentBox from '@/components/test/ContentBox'
@@ -8,74 +8,100 @@ import Section from '@/components/test/Section'
 
 const ResumePage = () => {
 
-
-  function zzz(obj: { a: number, b: number }) {
-    console.log(obj.a, obj.b)
-  }
-
-  zzz({ a: 3, b: 2 })
-
-  interface Props<T> {
-    a: string;
-    b: T
-  }
-
-  interface ChildProps<T> {
-    c: string;
-    d: T
-  }
+  const data = [
+    { url: 'asdasd', tag: ['tag', 'tag2', 'tag3'], title: 'title 1', littleTitle: 'subtitle hoho1' },
+    { url: 'asdasd', tag: ['tag'], title: 'title 2', littleTitle: 'subtitle hoho2' },
+    { url: 'asdasd', tag: ['tag', 'tag2', ], title: 'title 3', littleTitle: 'subtitle hoho3' },
+    { url: 'asdasd', tag: ['tag', 'tag2',], title: 'title 4', littleTitle: 'subtitle hoho4' },
+    { url: 'asdasd', tag: ['tag', 'tag2', ], title: 'title 5', littleTitle: 'subtitle hoho5' },
+    { url: 'asdasd', tag: ['tag', 'tag2', 'tag3'], title: 'title 6', littleTitle: 'subtitle hoho6' },
+    { url: 'asdasd', tag: ['tag2', 'tag3'], title: 'title 7', littleTitle: 'subtitle hoho7' },
+  ]
 
 
-  const obj: Props<ChildProps<number>> = {
-    a: 'asdasd',
-    b: {
-      c: 'asdasd',
-      d: 3
-    }
-  }
+  const [user, setUser] = useState({
+    name: '',
+    age: '',
+    hoho: '',
+  })
 
 
+  useEffect(() => {
+    console.log('user???', user)
+  }, [user])
 
-  let a: string = '213'
+  const handleChange = (e: any) => {
+    console.log(e.target)
 
+    // const {  } = e.target
 
-
-
-  const handlehoho = (e: Event) => {
-    console.log(e)
-  }
-
-
-
-
-  interface User<T> {
-    name: string;
-    data: T[]
-  }
-
-  interface UserData<T> {
-    data1: string;
-    data2: number;
-    data3?: T 
+    setUser(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value 
+      }
+    } )
   }
 
 
-  const user: User<UserData<string>> = {
-    name: 'jonghwan',
-    data: [{ data1: 'asdasd', data2: 33, data3: 'asdasd' }]
+  const handleSubmit = async() => {
+    const res = await axios.post('api url', { user: user });
+    const data = res.data
 
   }
-
-  const user2: User<UserData<number>> = {
-    name: 'jonghwan',
-    data: [{ data1: 'asdasd', data2: 33, data3: 33 }]
-  }
-
+  
 
   return (
     <div>
+      ResumePage
 
-      <button type="button" onClick={handlehoho}>asd</button>
+      <div>
+        <input type="text" value={user.name} name='name' placeholder='name' onChange={handleChange} className='text-black'/>
+        <input type="text" value={user.age} name='age' placeholder='age' onChange={handleChange} className='text-black'/>
+        <select name="hoho" id="" onChange={handleChange} className='text-black'>
+          <option value="hoho1" className='text-black'>hoho1</option>
+          <option value="hoho2" className='text-black'>hoho2</option>
+          <option value="hoho3" className='text-black'>hoho3</option>
+        </select>
+        <button type='button' onClick={handleSubmit} >서버로 보내기</button>
+      </div>
+
+
+
+      <Section>
+        칠드런 넘겨줌
+      </Section>
+
+      <Section>
+        가나다라
+      </Section>
+
+      <Section>
+        마바사
+      </Section>
+
+
+      <hr />
+      <br /> <br /> 
+
+      <div  className='flex flex-wrap gap-[5px]'> 
+        {data.map((item, idx) => {
+          return (
+              <ContentBox key={idx} data={item} /> 
+          )
+        })}
+      </div>
+
+      <hr />
+      <br /><br />
+
+
+      <RefParent />
+
+      <br /><br /><br />
+
+      <AsyncCompo />
+
       <h2>padding</h2>
       <div className='p-4 bg-blue-100'>p-4 px-4 py-4 pl-4 pr-4 pb-4 pt-4</div>
       <h2>margin</h2>
@@ -103,7 +129,7 @@ const ResumePage = () => {
       <h2>display</h2>
       <div>block inline</div>
 
-    
+    asdasd
 
 
       
